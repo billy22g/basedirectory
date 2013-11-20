9672a0f9dbf9f788d834d8f21d5880795d6532f8
Billy <navyosu@gmail.com>
2013-11-20 16:34:31 -0700

    Added placeholder image

--- app/helpers/bases_helper.rb
+++ app/helpers/bases_helper.rb
@@ -1,2 +1,10 @@
 module BasesHelper
+  def image_for(base)
+    if base.image_filename.blank?
+      image_tag 'placeholder.jpg'
+    else
+      image_tag base.image_filename
+    end
+  end
+
 end
