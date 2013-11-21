83d7d15837300b8ad5f8a235a6405a5b24594319
Billy <navyosu@gmail.com>
2013-11-21 16:38:48 -0700

    Added some javascript functionality for practice, but commented out for real, and added delete on index page

--- app/assets/javascripts/bases.js
+++ app/assets/javascripts/bases.js
@@ -1,12 +1,14 @@
-$(document).on('ready', function() {
-  $(".delete").bind("click", function(event) {
-    console.log("target", event.currentTarget);
-    event.preventDefault();
-    event.stopPropagation();
-    var $target = $(event.currentTarget);
+// $(document).on('ready', function() {
+//   $(".delete").bind("click", function(event) {
+//     console.log("target", event.currentTarget);
+//     event.preventDefault();
+//     event.stopPropagation();
+//     var $target = $(event.currentTarget);
 
-    var url = $target.attr("href");
+//     var url = $target.attr("href");
 
-    $.ajax(url, { type: "delete" });
-  });
-});
+//     $target.addClass("deleted");
+
+//     $.ajax(url, { type: "delete" });
+//   });
+// });
